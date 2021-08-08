741b6454c75ae85bc2cb46e96ce100f8db885d42
Henry Cagnini <henry.cagnini@gmail.com>
2021-08-08 09:31:31 -0300

    adaptation of script code

--- holdout/scripts/holdout_postprocess.py
+++ holdout/scripts/holdout_postprocess.py
@@ -4,6 +4,8 @@ import subprocess
 import pandas as pd
 import numpy as np
 
+from pandas.errors import EmptyDataError
+
 from sklearn.metrics import balanced_accuracy_score
 
 
@@ -26,21 +28,27 @@ def main(experiment_path: str, n_trials: int):
                 continue
 
             for some_file in file_preds:
-                local_df = pd.read_csv(os.path.join(experiment_path, experiment, dataset, 'overall', some_file), sep=';')
-                y_column = local_df.columns[0]
-                clf_column = local_df.columns[-1]
+                try:
+                    local_df = pd.read_csv(os.path.join(experiment_path, experiment, dataset, 'overall', some_file), sep=';')
+                    y_column = local_df.columns[0]
+                    clf_column = local_df.columns[-1]
 
-                y_true = local_df[y_column].values
-                pred_scores = np.array(local_df[clf_column].apply(lambda x: list(map(float, x.split(',')))).values.tolist())
-                y_pred = pred_scores.argmax(axis=1)
+                    y_true = local_df[y_column].values
+                    pred_scores = np.array(local_df[clf_column].apply(lambda x: list(map(float, x.split(',')))).values.tolist())
+                    y_pred = pred_scores.argmax(axis=1)
 
-                balanced_acc = balanced_accuracy_score(y_true, y_pred)
-                scores_per_dataset[dataset] += [balanced_acc]
+                    balanced_acc = balanced_accuracy_score(y_true, y_pred)
+                    scores_per_dataset[dataset] += [balanced_acc]
+                except EmptyDataError:  # file is there but is empty
+                    if os.path.exists(os.path.join(overall_folder, 'summary_1.csv')):
+                        os.remove(os.path.join(overall_folder, 'summary_1.csv'))
+                    del scores_per_dataset[dataset]
+                    break
 
     frozen_datasets = list(scores_per_dataset.keys())
 
     all_df = pd.DataFrame(
-        dtype=np.float,
+        dtype=float,
         index=frozen_datasets,
         columns=['mean', 'std']
     )
@@ -50,6 +58,7 @@ def main(experiment_path: str, n_trials: int):
         all_df.loc[dataset, 'std'] = np.std(scores_per_dataset[dataset])
 
     all_df.to_csv(os.path.join(experiment_path, 'holdout_summarized.csv'))
+    print(all_df)
 
 
 if __name__ == '__main__':
